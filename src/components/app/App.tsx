d95841b1dd130b7a584c6104749a24bb9713e417
Egor <fishmastor4@gmail.com>
2024-07-27 10:27:19 +0200

    add: new develop config

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function App({}: Props) {
   return (
-    <BrowserRouter basename="/brew_app_client">
+    <BrowserRouter>
         <Routes>
           <Route path='/' element={<MainPage/>}/>
           <Route path='/brewery/:id' element={<ItemPage/>}/>
